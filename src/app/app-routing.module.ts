e563c5b80aced7593f46642abcb3e5c66db25008
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-08-09 14:43:23 +0300

    config router for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { FatoraFormComponent } from './home/fatora-form/fatora-form.component';
-import { FinalStepComponent } from './home/final-step/final-step.component';
-import { ToolsSelctionsComponent } from './home/tools-selctions/tools-selctions.component';
+import { FatoraFormComponent } from './fatora-form/fatora-form.component';
+
+import { FinalStepComponent } from './final-step/final-step.component';
+import { ToolsSelctionsComponent } from './tools-selctions/tools-selctions.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
-    children:[
-      { path: 'form', component: FatoraFormComponent },
-  { path: 'service-selection', component: ToolsSelctionsComponent },
-  { path: 'final-step', component: FinalStepComponent },
-    ]
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: 'fatora-form', pathMatch: 'full' },
+  { path: 'fatora-form', component: FatoraFormComponent },
+  { path: 'tools-selection', component: ToolsSelctionsComponent},
+  { path: 'final', component: FinalStepComponent }
 ];
 
 @NgModule({
