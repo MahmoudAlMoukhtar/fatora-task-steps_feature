20c8a885707618c25d28f424c5373e795ac55778
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-08-09 14:48:42 +0300

    Declare the main application components and http library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http'
+import { FatoraFormComponent } from './fatora-form/fatora-form.component';
+import { FinalStepComponent } from './final-step/final-step.component';
+import { ToolsSelctionsComponent } from './tools-selctions/tools-selctions.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent,FatoraFormComponent,ToolsSelctionsComponent,FinalStepComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
