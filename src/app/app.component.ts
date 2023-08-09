da5313dc34a1ddb4ed939c48397a52ebd7d1cb0f
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-08-09 14:47:08 +0300

    Add functionality to validat form and the next and skip buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { IFormCred } from './form-cred.model';
+import { FormServService } from './form-serv.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +13,16 @@ import { NavigationEnd, Router } from '@angular/router';
 export class AppComponent {
   currStep:number = 1
   finalStep:number = 3
-  constructor(private router:Router) {
+  steps:boolean =true
+  
+  constructor(private router:Router, private formServ:FormServService) {
     this.router.events.subscribe((event)=>{
       if(event instanceof NavigationEnd){
         this.updateCurrStep(event.urlAfterRedirects)
+        this.steps = event.urlAfterRedirects.includes("fatora-form") || event.urlAfterRedirects.includes("tools-selection")
       }
     })
+    
   }
 
   updateCurrStep(url:string){
@@ -27,7 +34,45 @@ export class AppComponent {
         this.currStep = 3
     }
   }
+
   getProgressRatio(){
     return this.currStep /this.finalStep
   }
+  
+goToNext(event:any) {
+  this.formServ.submitClicked=true
+  if(this.currStep ==1){
+    event.preventDefault();
+    // Perform form validation and submit if necessary
+    if(this.validateForm()){
+      // Navigate to the next step
+      this.router.navigate(['/tools-selection']);
+    }
+  }else if(this.currStep ==2){
+    // send request to server side
+    this.submitForm()
+    // Navigate to the next step
+    this.router.navigate(['/final']);
+  }
+}
+
+validateForm():boolean{
+  const checkCompOrProjName = this.formServ.fatoraFormCred.compOrProjName?.trim() == ''
+  const checkcompanyCountry = this.formServ.fatoraFormCred.country?.trim() == ''
+  const checkcompanyCurrency = this.formServ.fatoraFormCred.currency?.trim() == ''
+  if(checkCompOrProjName || checkcompanyCountry || checkcompanyCurrency){
+    return false;
+  }
+  return true
+}
+
+submitForm() {
+  
+  this.formServ.postForm()
+}
+
+skip(){
+      this.router.navigate(['/final']);
+}
+
 }
