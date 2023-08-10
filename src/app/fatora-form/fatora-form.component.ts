ac636a31b7d189de427f8c4d90319ee002a78dc7
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-08-10 07:46:23 +0300

    remove code commented

--- src/app/fatora-form/fatora-form.component.ts
+++ src/app/fatora-form/fatora-form.component.ts
@@ -16,25 +16,7 @@ constructor(public formServ:FormServService, private http:HttpClient) {
    this.fatoraFormCred = formServ.fatoraFormCred
 }
 
-// handleInput(event:any){
-//   const name:string = event.target.name
-//   const value:string = event.target.value
-//   switch(name){
-//     case "compOrProjName":
-//       this.fatoraFormCred.compOrProjName = value;
-//       break;
-//     case "companyField":
-//       this.fatoraFormCred.companyField = value;
-//       break;
-//     case "country":
-//       this.fatoraFormCred.country = value;
-//       break;
-//     case "currency":
-//       this.fatoraFormCred.currency = value;
-//       break;
-//   }
-//   console.log(this.fatoraFormCred)
-// }
+
 
 ngOnInit() {
 }
